33e31917730c71631313c8eaf0205401689ed755
SaikiranDavangamS <saikirandavangamsreedhar@gmail.com>
2025-06-28 23:56:30 +0530

    Route Handlers - EPISODE-05 Part1

--- src/app.js
+++ src/app.js
@@ -6,44 +6,34 @@ const app = express();
 
 
 
-// app.use("/hello",(req,res) => {
-//     res.send("Hello from Namaste Node js")
-// })
-// app.use("/dashboard",(req,res) => {
-//     res.send("Hello from Namaste Node js Dashboard")
-// })
 
-// app.use("/test",(req,res) => {
-//     res.send("Test route for validation")
-// })
+app.use("/user",
+    // Route Handler
+    (req,res,next) => {
+        console.log("RouteHandler1")
+        next();
+        // res.send("1st Response")
 
 
-// This will match only "get" HTTP method API calls to /user
-app.get("/user/:userID/:userName/:password",(req,res) => {
-    console.log("req>",req.params)
-    res.send([
-        {firstName:"Saikiran",
-        lastName:"Davangam Sreedhar"
-    },
-        {firstName:"Saikiran",
-        lastName:"Davangam Sreedhar"
-    },
-        {firstName:"Saikiran",
-        lastName:"Davangam Sreedhar"
-    },])
-})
-
-app.post("/user",(req,res)=>{
-    res.send("Data saved successfully to Database!")
-})
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Data saved successfully from Database!")
-})
+},(req,res,next) => {
+    console.log("RouteHandler2")
+          // res.send("2nd Response")
+    next()
+},(req,res,next) => {
+    console.log("RouteHandler2")
+    //   res.send("3rd Response")
+    next()
+},(req,res,next) => {
+    console.log("RouteHandler2")
+    //   res.send("4th Response")
+        next()
+}
+)
 
 // This will match all the HTTP methods API calls to /test
-app.use("/",(req,res) => {
-    res.send("Namaste Saikiran")
-})
+// app.use("/",(req,res) => {
+//     res.send("Namaste Saikiran")
+// })
 
 
 app.listen(7777,() => {
